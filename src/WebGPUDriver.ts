435d875c17ebfd27d4f39e0e520a54723774611d
Forrest Sun <internetboy0823@gmail.com>
2023-07-11 18:32:37 -0700

    upgrade dependencies and include webgpu typescript definition

---
+++ src/WebGPUDriver.ts
@@ -0,0 +1,8 @@
+async function initialize() {
+    const adapter = await navigator.gpu?.requestAdapter();
+    const device = await adapter?.requestDevice();
+    if (!device) {
+        fail('need a browser that supports WebGPU');
+        return;
+    }
+}
