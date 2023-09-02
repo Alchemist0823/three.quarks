5aaea069257a19efcb2370c4dbf5b25f0dabf0d0
Forrest Sun <internetboy0823@gmail.com>
2023-09-01 21:54:28 -0700

    make color of console print default color

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-export * from "./ParticleEmitter";
-export * from "./Particle";
-export * from "./ParticleSystem";
-export * from "./VFXBatch";
-export * from "./SpriteBatch";
-export * from "./TrailBatch";
-export * from "./BatchedRenderer";
-export * from "./BatchedParticleRenderer";
-export * from "./QuarksLoader";
-export * from "./TypeUtil";
-export * from "./Plugin";
+export * from './ParticleEmitter';
+export * from './Particle';
+export * from './ParticleSystem';
+export * from './VFXBatch';
+export * from './SpriteBatch';
+export * from './TrailBatch';
+export * from './BatchedRenderer';
+export * from './BatchedParticleRenderer';
+export * from './QuarksLoader';
+export * from './TypeUtil';
+export * from './Plugin';
 
-export * from "./shape/";
-export * from "./functions/";
-export * from "./behaviors/";
-export * from "./sequencers/";
-export * from "./nodes/";
+export * from './shape/';
+export * from './functions/';
+export * from './behaviors/';
+export * from './sequencers/';
+export * from './nodes/';
 
-console.log("%c Powered by three.quarks. https://quarks.art/", "color: #ff0000; font-size: 16px; font-weight: bold;");
+// remove this line if you have pro license
+console.log('%c Powered by three.quarks. https://quarks.art/', 'font-size: 16px; font-weight: bold;');
