11aa608c96ededd7969f81748349fdf8a3d9ad80
Forrest Sun <internetboy0823@gmail.com>
2023-10-22 13:58:11 -0700

    add euler order option in euler generator

--- src/functions/EulerGenerator.ts
+++ src/functions/EulerGenerator.ts
@@ -1,26 +1,35 @@
-import {RotationGenerator} from "./RotationGenerator";
-import {Euler, Quaternion} from "three";
-import {FunctionValueGenerator, ValueGenerator, ValueGeneratorFromJSON} from "./ValueGenerator";
-import {FunctionJSON} from "./FunctionJSON";
+import {RotationGenerator} from './RotationGenerator';
+import {Euler, Quaternion} from 'three';
+import {FunctionValueGenerator, ValueGenerator, ValueGeneratorFromJSON} from './ValueGenerator';
+import {FunctionJSON} from './FunctionJSON';
+import {EulerOrder} from 'three/src/math/Euler';
 
 export class EulerGenerator implements RotationGenerator {
+    type: 'rotation';
+    eular: Euler;
 
-    type: "rotation";
-    constructor(public angleX: FunctionValueGenerator | ValueGenerator, public angleY: FunctionValueGenerator | ValueGenerator, public angleZ: FunctionValueGenerator | ValueGenerator) {
-        this.type = "rotation";
+    constructor(
+        public angleX: FunctionValueGenerator | ValueGenerator,
+        public angleY: FunctionValueGenerator | ValueGenerator,
+        public angleZ: FunctionValueGenerator | ValueGenerator,
+        eulerOrder?: EulerOrder
+    ) {
+        this.type = 'rotation';
+        this.eular = new Euler(0, 0, 0, eulerOrder);
     }
 
     genValue(quat: Quaternion, t?: number): Quaternion {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        return quat.setFromEuler(new Euler(this.angleX.genValue(t!), this.angleY.genValue(t!), this.angleZ.genValue(t!)));
+        this.eular.set(this.angleX.genValue(t!), this.angleY.genValue(t!), this.angleZ.genValue(t!));
+        return quat.setFromEuler(this.eular);
     }
 
     toJSON(): FunctionJSON {
         return {
-            type: "Euler",
+            type: 'Euler',
             angleX: this.angleX.toJSON(),
             angleY: this.angleY.toJSON(),
             angleZ: this.angleZ.toJSON(),
+            eulerOrder: this.eular.order,
         };
     }
 
@@ -29,10 +38,11 @@ export class EulerGenerator implements RotationGenerator {
             ValueGeneratorFromJSON(json.angleX) as FunctionValueGenerator,
             ValueGeneratorFromJSON(json.angleY) as FunctionValueGenerator,
             ValueGeneratorFromJSON(json.angleZ) as FunctionValueGenerator,
+            json.eulerOrder as EulerOrder
         );
     }
 
     clone(): RotationGenerator {
-        return new EulerGenerator(this.angleX, this.angleY, this.angleZ);
+        return new EulerGenerator(this.angleX, this.angleY, this.angleZ, this.eular.order);
     }
 }
