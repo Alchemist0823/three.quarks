b8295bc88bbe81245414d3f9eb7de914ac2aaf32
Forrest Sun <internetboy0823@gmail.com>
2023-10-24 10:03:08 -0700

    fix color range according to unity definition

--- src/functions/ColorRange.ts
+++ src/functions/ColorRange.ts
@@ -1,23 +1,25 @@
+import {ColorGenerator, FunctionColorGenerator} from './ColorGenerator';
+import {FunctionJSON} from './FunctionJSON';
+import {ColorToJSON, JSONToColor} from '../util/JSONUtil';
+import {Vector4} from 'three';
 
-import { FunctionColorGenerator} from "./ColorGenerator";
-import {FunctionJSON} from "./FunctionJSON";
-import {ColorToJSON, JSONToColor} from "../util/JSONUtil";
-import {Vector4} from "three";
-
-export class ColorRange implements FunctionColorGenerator {
-    constructor(public a: Vector4, public b: Vector4) {
-        this.type = "function";
+export class ColorRange implements ColorGenerator {
+    constructor(
+        public a: Vector4,
+        public b: Vector4
+    ) {
+        this.type = 'value';
     }
 
-    genColor(color: Vector4, t: number): Vector4 {
-        return color.copy(this.a).lerp(this.b, t);
+    genColor(color: Vector4, t?: number): Vector4 {
+        return color.copy(this.a).lerp(this.b, Math.random());
     }
 
-    type: "function";
+    type: 'value';
 
     toJSON(): FunctionJSON {
         return {
-            type: "ColorRange",
+            type: 'ColorRange',
             a: ColorToJSON(this.a),
             b: ColorToJSON(this.b),
         };
@@ -27,7 +29,7 @@ export class ColorRange implements FunctionColorGenerator {
         return new ColorRange(JSONToColor(json.a), JSONToColor(json.b));
     }
 
-    clone(): FunctionColorGenerator {
+    clone(): ColorGenerator {
         return new ColorRange(this.a.clone(), this.b.clone());
     }
 }
